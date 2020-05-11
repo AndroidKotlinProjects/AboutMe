304d882dcbbeba67d630a60235aa1fe0433b60b7
thequinneffect@desktop <n_quinny12@hotmail.com>
2020-05-11 23:44:39 +1000

    initial commit

---
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.android.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.android.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val names: Names = Names("Nicholas Quinn")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
+        binding.names = names
+    }
+
+    private fun addNickname(view: View) {
+
+        binding.apply {
+            names?.nickname = binding.nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+        //nameText.gravity = Gravity.CENTER
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ app/src/main/java/com/example/android/aboutme/Names.kt
@@ -0,0 +1,3 @@
+package com.example.android.aboutme
+
+data class Names(var name: String = "", var nickname: String = "")
